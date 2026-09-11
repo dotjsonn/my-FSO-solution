e668e41119d69d5a0f09f9464c850e23a2793642
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-09-11 23:37:14 +0800

    feat: add LoginForm component

---
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,29 @@
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleLogin}>
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            value={props.username}
+            onChange={props.handleUsername}
+            />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            value={props.password}
+            onChange={props.handlePassword}
+            />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}
+
+export default LoginForm
