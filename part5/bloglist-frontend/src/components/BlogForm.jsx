ae96e89b367c14e1e899f1c7d44c8caec2082767
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-09-11 23:55:39 +0800

    refactor: add the heading of creating blog

--- part5/bloglist-frontend/src/components/BlogForm.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -18,36 +18,39 @@ const BlogForm = ({createBlog}) => {
   }
 
   return (
-    <form onSubmit={handleBlog}>
-      <div>
-        <label>
-          title:
-          <input 
-            type="text"
-            value={title}
-            onChange={({target}) => setTitle(target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          author:
-          <input 
-            type="text" 
-            value={author}
-            onChange={({target}) => setAuthor(target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          url:
-          <input 
-            type="text"
-            value={url}
-            onChange={({target}) => setUrl(target.value)} />
-        </label>
-      </div>
-      <button type="submit">create</button>
-    </form>
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleBlog}>
+        <div>
+          <label>
+            title:
+            <input
+              type="text"
+              value={title}
+              onChange={({target}) => setTitle(target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            author:
+            <input
+              type="text"
+              value={author}
+              onChange={({target}) => setAuthor(target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            url:
+            <input
+              type="text"
+              value={url}
+              onChange={({target}) => setUrl(target.value)} />
+          </label>
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
   )
 }
 
