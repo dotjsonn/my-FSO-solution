4971e006a254604221cfa372b8374a08330884fe
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-09-14 12:01:01 +0800

    feat: add handle likes functionality

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -71,6 +71,16 @@ const App = () => {
     }, 5000);
   }
 
+  const updateLikes = async (id) => {
+    const blog = blogs.find(blog => blog.id === id)
+    const blogObject = {
+      ...blog,
+      likes: blog.likes + 1
+    }
+    const updatedBlog = await blogService.update(blogObject, id)
+    setBlogs(blogs.map(blog => blog.id === id ? updatedBlog : blog))
+  }
+
   const loginForm = () => (
     <div>
       <h2>log in to application</h2>
@@ -93,7 +103,7 @@ const App = () => {
   const displayBlog = () => (
     <div>
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} user={user} />
+        <Blog key={blog.id} blog={blog} user={user} updateLikes={updateLikes} />
       )}
     </div>
   )
