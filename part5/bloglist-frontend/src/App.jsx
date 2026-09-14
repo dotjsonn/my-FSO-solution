e6e2153ed4e9c7d01e7ad197bda902801d83139d
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-09-14 11:16:52 +0800

    feat: add user in props for Blog component

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -93,7 +93,7 @@ const App = () => {
   const displayBlog = () => (
     <div>
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} user={user} />
       )}
     </div>
   )
