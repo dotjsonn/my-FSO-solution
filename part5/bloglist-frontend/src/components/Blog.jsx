e27606a2fb9d94bd346a53cd487984a160b604f0
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-09-14 11:16:14 +0800

    feat: add togglable to hide and show full details of blog

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog, user }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [showDetails, setShowDetails] = useState(false)
+
+  const show = {display: showDetails ? '' : 'none'}
+
+  const changeVisibility = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const label = showDetails ? 'hide' : 'view'
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={changeVisibility}>{label}</button>
+      </div>
+      <div style={show}>
+        <div>{blog.url}</div>
+        <div>likes 0</div>
+        <div>{user.name || user.username}</div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
